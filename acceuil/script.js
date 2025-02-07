ffa4dd47075ae6d7cb6bd67fab99145e856cf579
yvesgroovert <yvesgroovert@icloud.com>
2025-02-07 13:06:52 +0100

    Add files via upload

---
+++ acceuil/script.js
@@ -0,0 +1,3 @@
+//  Récupère ton élément avec querySelector
+// Tu appliques un addEVENTlistener dessus au click
+// Tu zjoutes une classe qui fait grossir ton élément avec addClass ou toggle qui va ajouter ou retirer en cas d existance ou non
